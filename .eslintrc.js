6bc7962bec2947ba09dc6884a218deb2862a93ad
Ryan Bruns <ryan.bruns7@gmail.com>
2021-10-18 15:38:46 -0600

    lerna to manage

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  ignorePatterns: ['build', '.*.js', '*.config.js', 'node_modules'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-use-before-define': 'warn',
+    'import/no-extraneous-dependencies': ['error'],
+    'import/no-self-import': 'error',
+    'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
+    'import/prefer-default-export': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/prop-types': 'off',
+  },
+};
